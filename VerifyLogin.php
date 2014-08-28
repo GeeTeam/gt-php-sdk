2e7d21c650850708cc1558f5842018541e622586
zheng <dreamzsm@gmail.com>
2014-08-28 10:51:46 +0800

    封闭成一个类

--- VerifyLogin.php
+++ VerifyLogin.php
@@ -4,8 +4,9 @@
  * 本文件示例只是简单的输出 Yes or No
  */
 require_once('lib/geetestlib.php');
-
-$validate_response = geetest_validate(@$_POST['geetest_challenge'], @$_POST['geetest_validate'], @$_POST['geetest_seccode']);
+$PRIVATE_KEY = ("0f1a37e33c9ed10dd2e133fe2ae9c459"
+$geetestdemo = new geetestdemo($PRIVATE_KEY);
+$validate_response = $geetestdemo->geetest_validate(@$_POST['geetest_challenge'], @$_POST['geetest_validate'], @$_POST['geetest_seccode']);
 if ($validate_response) {
 	echo 'Yes!';
 } else {
--- lib/geetestlib.php
+++ lib/geetestlib.php
@@ -3,60 +3,64 @@
  * 极验行为式验证安全平台，php 网站主后台包含的库文件
  */
 
-define("PRIVATE_KEY","0f1a37e33c9ed10dd2e133fe2ae9c459");
+// define("PRIVATE_KEY","0f1a37e33c9ed10dd2e133fe2ae9c459");
+
+class geetestdemo{
+	function __construct($PRIVATE_KEY){
+		$this->PRIVATE_KEY = $PRIVATE_KEY;
+	}
 
 
+	function geetest_validate($challenge, $validate, $seccode) {	
+		$apiserver = 'api.geetest.com';
+		if (strlen($validate) > 0 && $this->_check_result_by_private($challenge, $validate)) {		
+			$query = 'seccode='.$seccode;
+			$servervalidate = $this->_http_post($apiserver, '/validate.php', $query);			
+			if (strlen($servervalidate) > 0 && $servervalidate == md5($seccode)) {
+				return TRUE;
+			}
+		}
+		
+		return FALSE;		
+	}
 
-function geetest_validate($challenge, $validate, $seccode) {	
-	$apiserver = 'api.geetest.com';
-	if (strlen($validate) > 0 && _check_result_by_private($challenge, $validate)) {		
-		$query = 'seccode='.$seccode;
-		$servervalidate = _http_post($apiserver, '/validate.php', $query);			
-		if (strlen($servervalidate) > 0 && $servervalidate == md5($seccode)) {
-			return TRUE;
+	function _check_result_by_private($origin, $validate) {
+		return $validate == md5($this->PRIVATE_KEY.'geetest'.$origin) ? TRUE : FALSE;
+	}
+
+	function _http_post($host, $path, $data, $port = 80) {
+		// $data = _fix_encoding($data);
+		
+		$http_request  = "POST $path HTTP/1.0\r\n";
+		$http_request .= "Host: $host\r\n";
+		$http_request .= "Content-Type: application/x-www-form-urlencoded\r\n";
+		$http_request .= "Content-Length: " . strlen($data) . "\r\n";
+		$http_request .= "\r\n";
+		$http_request .= $data;
+
+		$response = '';
+		if (($fs = @fsockopen($host, $port, $errno, $errstr, 10)) == false) {
+			die ('Could not open socket! ' . $errstr);
+		}
+		
+		fwrite($fs, $http_request);
+
+		while (!feof($fs))
+			$response .= fgets($fs, 1160);
+		fclose($fs);
+		
+		$response = explode("\r\n\r\n", $response, 2);
+		return $response[1];
+	}
+
+	function _fix_encoding($str) { 	
+		$curr_encoding = mb_detect_encoding($str) ; 
+		
+		if($curr_encoding == "UTF-8" && mb_check_encoding($str,"UTF-8")) {
+			return $str; 
+		} else {
+			return utf8_encode($str); 
 		}
 	}
-	
-	return FALSE;		
 }
-
-function _check_result_by_private($origin, $validate) {
-	return $validate == md5(PRIVATE_KEY.'geetest'.$origin) ? TRUE : FALSE;
-}
-
-function _http_post($host, $path, $data, $port = 80) {
-	// $data = _fix_encoding($data);
-	
-	$http_request  = "POST $path HTTP/1.0\r\n";
-	$http_request .= "Host: $host\r\n";
-	$http_request .= "Content-Type: application/x-www-form-urlencoded\r\n";
-	$http_request .= "Content-Length: " . strlen($data) . "\r\n";
-	$http_request .= "\r\n";
-	$http_request .= $data;
-
-	$response = '';
-	if (($fs = @fsockopen($host, $port, $errno, $errstr, 10)) == false) {
-		die ('Could not open socket! ' . $errstr);
-	}
-	
-	fwrite($fs, $http_request);
-
-	while (!feof($fs))
-		$response .= fgets($fs, 1160);
-	fclose($fs);
-	
-	$response = explode("\r\n\r\n", $response, 2);
-	return $response[1];
-}
-
-function _fix_encoding($str) { 	
-	$curr_encoding = mb_detect_encoding($str) ; 
-	
-	if($curr_encoding == "UTF-8" && mb_check_encoding($str,"UTF-8")) {
-		return $str; 
-	} else {
-		return utf8_encode($str); 
-	}
-}
-
 ?>
