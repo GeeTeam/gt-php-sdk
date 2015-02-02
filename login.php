210d78fba7f7766381a8e0ef6bed5d3811382324
tanxu <877077145@qq.com>
2015-02-02 22:44:40 +0800

    modify login.php

--- login.php
+++ login.php
@@ -1,54 +1,81 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
 	<title>极验行为式验证 php 类网站安装测试页面</title>
-	<style>	
-	body {
-		background-color: #9e9;
-	}
-	.wrap {
-		width: 960px;
-		margin: 100px auto;
-		font-size: 125%;
-	}
-	.row {
-		margin: 30px 0;
-	}
-	
-	</style>
 </head>
 <body>
-	<div class="wrap">
-		<h1>极验行为式验证 php 类网站安装测试页面</h1>
-		<form method="post" action="VerifyLogin.php">
-			<div class="row">
-				<label for="name">邮箱</label>
-				<input type="text" id="email" name="email" value="geetest@126.com"/>
-			</div>
-			<div class="row">
-				<label for="passwd">密码</label>
-				<input type="password" id="passwd" name="passwd" value="gggggggg"/>
-			</div>
-			<div class="row">
-				<?php
-				require_once("./lib/geetestlib.php");
-				$captcha_key = "a40fd3b0d712165c5d13e6f747e948d4";
-				$product = "embed";//float 、embed 、popup
-				$geetestlib = new geetestdemo($captcha_key,'');
-				if ($geetestlib->process() == 1) {
-					echo $geetestlib->geetest_api($product);
-				}else{
+	<style type="text/css">
 
-					echo "use your own captcha HTML web code!";//这里输出网站原有验证码
-				}
+		.container{
+			width: 960px;
+			margin: 0 auto;
+		}
+		.content{
+			width: 960px;
+			margin: 10 auto;
+			border-top: 1px solid #ccc;
+
+		}
+		.box{
+			width:300px;
+			margin: 30px auto; 
+		}
+		.header{
+			margin: 80px auto 30px auto;
+			text-align: center;
+			font-size: 34px;
+		}
+		input{
+			width: 200px;
+			padding: 6px 9px;
+		}
+		button{
+			cursor: pointer;
+			line-height: 35px;
+			width: 110px;
+			margin:30px 0 0 90px;
+			border: 1px solid #FFFFF0;
+			background-color: #31C552;
+
+			border-radius: 4px;
+			font-size: 14px;	
+			color: #FFFFF0;	
+		}
+	</style>
+
+	<div class="container">
+		<div class="header">
+			极验行为式验证 php 类网站安装测试页面
+		</div>
+		<div class="content">
+			<form method="post" action="VerifyLogin.php">
+				<div class="box">
+					<label>邮箱：</label>
+					<input type="text" name="email" value="geetest@geetest.com"/>
+				</div>
+				<div class="box">
+					<label>密码：</label>
+					<input type="password" name="password" value="geetest"/>
+				</div>
+				<div class="box">
+				<?php
+					require_once("./lib/geetestlib.php");
+					$captcha_key = "a40fd3b0d712165c5d13e6f747e948d4";
+					$product = "embed";//float 、embed 、popup
+					$geetestlib = new geetestdemo($captcha_key,'');
+					if ($geetestlib->process() == 1) {
+						echo $geetestlib->geetest_api($product);
+					}else{
+
+						echo "use your own captcha HTML web code!";//这里输出网站原有验证码
+					}
 				 ?>
-				
-			</div>
-			<div class="row">
-				<input type="submit" value="登录" />
-			</div>
-		</form>
-	</div>	
+				</div>
+				<div class="box">
+					<button>提交</button>
+				</div>
+			</form>
+		</div>
+	</div>
 </body>
 </html>
