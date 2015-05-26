a097c363446ed562724bf884af3da9443f152681
tanxu <877077145@qq.com>
2015-05-26 11:56:43 +0800

    Change the transport

--- src/class.geetest.php
+++ src/class.geetest.php
@@ -2,28 +2,21 @@
 /**
  * 极验行为式验证安全平台，php 网站主后台包含的库文件
  */
-
 class Geetest{
-
 	const GT_API_SERVER  = 'http://api.geetest.com';
 	const GT_SSL_SERVER  = 'https://api.geetest.com';
 	const GT_SDK_VERSION  = 'php_2.15.4.2.2';
-
 	private $captcha_id;
 	private $private_key;
-
 	public function __construct() {
 		$this->challenge = "";
 	}
-
 	public function set_captchaid($captcha_id) {
 		$this->captcha_id = $captcha_id;
 	}
-
 	public function set_privatekey($private_key) {
 		$this->private_key = $private_key;
 	}
-
 	public function register() {
 		$this->challenge = $this->send_request("/register.php", array("gt"=>$this->captcha_id));
 		if (strlen($this->challenge) != 32) {
@@ -31,7 +24,6 @@ class Geetest{
 		}
 		return 1;
 	}
-
 	public function get_widget($product, $popupbtnid = "", $ssl = FALSE) {
 		$params = array(
 			"gt" => $this->captcha_id,
@@ -44,13 +36,16 @@ class Geetest{
 		$server = $ssl ? self::GT_SSL_SERVER : self::GT_API_SERVER;
 		return "<script type='text/javascript' src='". $server ."/get.php?". http_build_query($params) ."'></script>";
 	}
-
 	public function validate($challenge, $validate, $seccode) {
 		if ( ! $this->check_validate($challenge, $validate)) {
 			return FALSE;
 		}
-		$query = http_build_query(array("seccode"=>$seccode, "sdk"=>self::GT_SDK_VERSION));
-		$codevalidate = $this->http_post('api.geetest.com', '/validate.php', $query);
+		$data = array(
+			"seccode"=>$seccode,
+			"sdk"=>GT_SDK_VERSION,
+		);
+		$url = "http://api.geetest.com/validate.php";
+		$codevalidate = $this->_request($url, $data);
 		if (strlen($codevalidate) > 0 && $codevalidate == md5($seccode)) {
 			return TRUE;
 		} else if ($codevalidate == "false"){
@@ -59,7 +54,6 @@ class Geetest{
 			return $codevalidate;
 		}
 	}
-
 	private function check_validate($challenge, $validate) {
 		if (strlen($validate) != 32) {
 			return FALSE;
@@ -69,7 +63,6 @@ class Geetest{
 		}
 		return TRUE;
 	}
-
 	private function send_request($path, $data, $method = "GET") {
 		if ($method == "GET") {
 			$opts = array(
@@ -83,24 +76,36 @@ class Geetest{
 			return $response;
 		}
 	}
-
-	private function http_post($host,$path,$data,$port = 80){
-		$http_request = "POST $path HTTP/1.0\r\n";
-		$http_request .= "Host: $host\r\n";
-		$http_request .= "Content-Type: application/x-www-form-urlencoded\r\n";
-		$http_request .= "Content-Length: " . strlen($data) . "\r\n";
-		$http_request .= "\r\n";
-		$http_request .= $data;
-		$response = '';
-		if (($fs = @fsockopen($host, $port, $errno, $errstr, 10)) == false) {
-			die ('Could not open socket! ' . $errstr);
-		}		
-		fwrite($fs, $http_request);
-		while (!feof($fs))
-			$response .= fgets($fs, 1160);
-		fclose($fs);		
-		$response = explode("\r\n\r\n", $response, 2);
-		return $response[1];
-	}
+	    private function _request($url, $postdata = null){
+	    	$data = http_build_query($postdata);
+	    	if(function_exists('curl_exec')){
+	    		$ch = curl_init();
+	    		curl_setopt($ch, CURLOPT_URL, $url);
+	    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	    		if(!$postdata){
+	    			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	    			curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']);
+	    		}else{
+	    			curl_setopt($ch, CURLOPT_POST, 1);
+	    			curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
+	    		}
+	    		$data = curl_exec($ch);
+	    		curl_close($ch);
+	    	}else{
+	    		if($postdata){
+		    		$url = $url.'?'.$data;
+				$opts = array(
+					'http' => array (
+			            		'method' => 'POST',
+			            		'header'=> "Content-type: application/x-www-form-urlencoded\r\n" . "Content-Length: " . strlen($data) . "\r\n",
+			            		'content' => $data
+			            		)
+				    );
+				$context = stream_context_create($opts);
+		    		$data = file_get_contents($url, false, $context);
+	    		}
+	    	}
+    	return $data;
+    }
 }
 ?>
