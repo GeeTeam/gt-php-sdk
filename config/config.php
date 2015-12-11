13b89fcf79926b5c28080d8cca0bfc537befebbf
Latrell Chan <i@latrell.me>
2015-12-12 01:01:23 +0800

    Update config.php
    支持在外部定义配置文件。

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@
 *  Geetest配置文件
 * @author Tanxu
 */
-define("CAPTCHA_ID", "3386e03c620a4067f18fa92c370f1594");
-define("PRIVATE_KEY", "5fe89444b54d3a3b8e49594c42a770cf");
+defined('CAPTCHA_ID') or define('CAPTCHA_ID', '3386e03c620a4067f18fa92c370f1594');
+defined('PRIVATE_KEY') or define('PRIVATE_KEY', '5fe89444b54d3a3b8e49594c42a770cf');
 
  ?>
